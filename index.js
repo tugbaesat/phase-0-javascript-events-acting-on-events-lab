19a1d043203582636204c417d1f6ab0578953f10
Tugba Esat <tugbaesat007@gmail.com>
2023-04-17 18:58:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+document.addEventListener("keydown", function(event){
+if (event.key == "ArrowLeft") {
+moveDodgerLeft();
+}
+if (event.key == "ArrowRight") {
+    moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
